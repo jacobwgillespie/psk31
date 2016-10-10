2450b66dd0e09f3567245b433ac51dad0b450f65
Jacob Gillespie <jacobwgillespie@gmail.com>
2016-10-10 10:26:54 -0500

    More progress towards proper amplitude diminishing

--- src/app.js
+++ src/app.js
@@ -1,14 +1,19 @@
-import { messageToBits } from './utils';
+import { bitsToPhases, messageToBits, phasesToAmp } from './utils';
 
 import { buildBinaryPhaseShiftKeyingOscillator } from './psk';
 
 const ENERGY_PER_BIT = 100;
-const CARRIER_FREQUENCY = 1200;
+const CARRIER_FREQUENCY = 1000;
 const BPSK31_SAMPLE_RATE = 44100;
 const BAUD_RATE = 31.25;
 const BIT_DURATION = BPSK31_SAMPLE_RATE / BAUD_RATE;
-const PAYLOAD = 'The variable-length coding used in the BPSK system was chosen by collecting a large volume of English language ASCII text files and analysing them to establish the occurrence-frequency of each of the 128 ASCII characters.';
+const PAYLOAD = 'CQ CQ CQ de KD5TEN KD5TEN KD5TEN pse k';
 const PAYLOAD_BITS = messageToBits(PAYLOAD);
+
+// const PAYLOAD_BITS = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+const PAYLOAD_PHASES = bitsToPhases(PAYLOAD_BITS);
+const PAYLOAD_AMPLITUDE_MODE = phasesToAmp(PAYLOAD_PHASES);
+console.log('bits', PAYLOAD_BITS, 'phases', PAYLOAD_PHASES, 'amp', PAYLOAD_AMPLITUDE_MODE);
 
 const HALF_PI = Math.PI / 2;
 
@@ -18,8 +23,6 @@ const bpsk31 = buildBinaryPhaseShiftKeyingOscillator(
 
 (() => {
   let startTime;
-  let currentPhase = 0;
-
   let previousBit = 0;
 
   const calculateBit = (time, mtime) => {
@@ -33,9 +36,9 @@ const bpsk31 = buildBinaryPhaseShiftKeyingOscillator(
     return bit;
   };
 
-  const calculatePhase = (time, mtime) => {
+  const calculatePhase = (time, mtime, currentPhase) => {
     if (calculateBit(time, mtime) === 0 && mtime > 15.625) {
-      currentPhase = currentPhase === 0 ? 1 : 0;
+      return currentPhase === 0 ? 1 : 0;
     }
 
     return currentPhase;
@@ -44,30 +47,20 @@ const bpsk31 = buildBinaryPhaseShiftKeyingOscillator(
   let previousMode = 0;
   let previousModifier = 1;
 
-  const calculateAmpModifier = (time, mtime) => {
-    let modifier = 1;
-    let mode = previousMode;
-    const bit = calculateBit(time, mtime);
+  const calculateAmpModifier = (mode, mtime) => {
+    switch (mode) {
+      case 0:
+        return 1;
 
-    if (bit === 1) {
-      modifier = 1;
-      mode = 0;
-    } else if (mtime < 15.625) {
-      modifier = Math.cos(HALF_PI * (mtime / 15.625));
-      mode = 1;
-    } else {
-      modifier = Math.cos((3 * HALF_PI) + (HALF_PI * ((mtime - 15.625) / 15.625)));
-      mode = 2;
+      case 1:
+        return Math.cos(HALF_PI * (mtime / 15.625));
+
+      case 2:
+        return Math.cos((3 * HALF_PI) + (HALF_PI * ((mtime - 15.625) / 15.625)));
+
+      default:
+        return 1;
     }
-
-    if (mode !== previousMode) {
-      // console.log(time, 'change', mode, modifier, previousModifier, bit, mtime);
-    }
-
-    previousMode = mode;
-    previousModifier = modifier;
-
-    return modifier;
   };
 
   const ctx = new AudioContext(1, 0, BPSK31_SAMPLE_RATE);
@@ -75,14 +68,20 @@ const bpsk31 = buildBinaryPhaseShiftKeyingOscillator(
   const spn = ctx.createScriptProcessor(16384, 1, 1);
   spn.onaudioprocess = (audioProcessingEvent) => {
     const outputData = audioProcessingEvent.outputBuffer.getChannelData(0);
+    let currentPhase = 0;
     startTime = startTime || audioProcessingEvent.playbackTime;
     for (let sample = 0; sample < outputData.length; sample += 1) {
       const t = (audioProcessingEvent.playbackTime - startTime) + (sample / ctx.sampleRate);
 
       const mtime = (t * 1000) % 31.25;
 
-      const i = calculatePhase(t, mtime);
-      const ampModifier = calculateAmpModifier(t, mtime);
+      const idx = Math.floor((t * 1000) / 15.625);
+      const i = PAYLOAD_BITS[idx];
+      const ampMode = PAYLOAD_AMPLITUDE_MODE[idx];
+
+      // const i = calculatePhase(t, mtime, currentPhase);
+      // currentPhase = i;
+      const ampModifier = calculateAmpModifier(ampMode, mtime);
       const result = bpsk31(t, i, ampModifier);
       outputData[sample] = result;
     }
--- src/utils.js
+++ src/utils.js
@@ -4,10 +4,10 @@ export const messageToBits = message =>
   message.split('').reduce(
     (bits, character) => bits.concat(VARICODE[character] || []).concat([0, 0]),
     []
-  );
+  ).concat([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
 
 export const bitsToPhases = (bits) => {
-  const phases = [0, 1, 0, 1, 0, 1]; // 000
+  const phases = [0, 1, 1, 0, 0, 1]; // 000
   bits.forEach((bit) => {
     const previousPhase = phases[phases.length - 1];
     const differentPhase = previousPhase === 0 ? 1 : 0;
@@ -21,4 +21,23 @@ export const bitsToPhases = (bits) => {
   });
 
   return phases;
+};
+
+export const phasesToAmp = (phases) => {
+  const amp = [];
+
+  for (let i = 0; i < phases.length - 1; i += 2) {
+    const first = phases[i];
+    const second = phases[i + 1];
+
+    if (first !== second) {
+      amp.push(1);
+      amp.push(2);
+    } else {
+      amp.push(0);
+      amp.push(0);
+    }
+  }
+
+  return amp;
 };
